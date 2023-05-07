a1ceb1ab57e79d608af67ad8ae1e5eef093ff7a1
JISOLPARK <108747841+jip007@users.noreply.github.com>
2023-05-07 11:30:14 -0400

    finished explore.js

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,12 +1,39 @@
 // explore.js
 
 window.addEventListener('DOMContentLoaded', init);
-const voiceSelect = documnet.getElementById('voice-select');
-let voices = []
+const voiceSelect = document.getElementById('voice-select');
+let voices = [];
 const speech = window.speechSynthesis;
-voices = speech.getVoices();
-
+const talkButton = document.querySelector('button');
 
 function init() {
+    // loading the voices so that user can choose which voice they want to select
+  speech.onvoiceschanged = () => {
+    voices = speech.getVoices();
+    voices.forEach((voice) => {
+      const voiceOption = document.createElement('option');
+      voiceOption.textContent = `${voice.name} (${voice.lang})`;
+      voiceOption.setAttribute('value', voice.name);
+      voiceSelect.appendChild(voiceOption);
+    });
+  };
+    //when the Press to Talk buttom is clicked, the voice speaks
+    talkButton.addEventListener('click', () => {
+    const textToSpeak = document.getElementById('text-to-speak').value;
+    const selectedVoice = voiceSelect.value;
+    //does not talk it a voice is not selected
+    if (selectedVoice !== "select"){
+        // if the voice is selected, 
+        const talking = new SpeechSynthesisUtterance(textToSpeak);
+        const currVoice = voices.find(voice => voice.name === selectedVoice);
+        talking.voice = currVoice;
+        speech.speak(talking);
 
+        const smilingImg = document.querySelector('img');
+        smilingImg.setAttribute('src','assets/images/smiling-open.png');
+        talking.onend = () => {
+            smilingImg.setAttribute('src', 'assets/images/smiling.png');
+        };
+    }
+  });
 }
